cecbe683cf5fceb2cc81b0c234483a46edabeee5
MoisesDominguezVillegas <druidax66x@gmail.com>
2023-11-19 14:48:40 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ function App() {
   return (
     <div className="App">
       <div className='contenedor-principal'>
-        <h1>Esto es lo que dicen nuestros estudiantes sobre freeCodeCamp:</h1>
+        <h1>Esto es lo que dicen nuestros estudiantes sobre el Bootcamp:</h1>
         <Testimonio
           nombre="Shawn Wang"
           pais="Singapur"
           imagen="shawn"
           cargo="Ingeniero de Software"
           empresa="Amazon"
-          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida."
+          testimonio="Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en el Bootcamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. El Bootcamp cambió mi vida."
         />
 
         <Testimonio
@@ -21,7 +21,7 @@ function App() {
           imagen="sarah"
           cargo="Ingeniera de Software"
           empresa="ChatDesk"
-          testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrolllador en una empresa increíble."
+          testimonio="El Bootcamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrolllador en una empresa increíble."
         />
 
         <Testimonio
@@ -30,7 +30,7 @@ function App() {
           imagen="emma"
           cargo="Ingeniera de Software"
           empresa="Spotify"
-          testimonio="Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algorítmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify"
+          testimonio="Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso del Bootcamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algorítmos en Bootcamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify"
         />
       </div>
     </div>
